98a341a4a7a2c3cde4e45d0de5abe9c4dba86195
Matthew Thomas <matthewthomas@Marvin.local>
2019-02-11 22:23:18 +0000

    fixed overloading. Added defensive checks

--- Telegram.cs
+++ Telegram.cs
@@ -5,10 +5,18 @@ namespace DotNetStandard_Telegram {
     public class Telegram {
         private string _bot;
         private string _group;
-        private HttpClient _http = new HttpClient();
-        public Telegram(string strBot, string strGroup) {
-            this._bot = strBot;
-            this._group = strGroup;
+        private HttpClient _http = new HttpClient ();
+        public Telegram (string strBot, string strGroup) {
+            if (!String.IsNullOrEmpty (strBot)) {
+                this._bot = strBot;
+            } else {
+                throw new ArgumentNullException ("No bot api key provided");
+            }
+            if (!String.IsNullOrEmpty (strGroup)) {
+                this._group = strGroup;
+            } else {
+                throw new ArgumentNullException ("No group ID provided");
+            }
         }
 
         /// <summary>
@@ -16,12 +24,15 @@ namespace DotNetStandard_Telegram {
         /// </summary>
         /// <param name="strMessage">Message content to be sent</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool SendMessage(string strMessage) {
+        public bool sendMessage (string strMessage) {
+            if(string.IsNullOrEmpty(strMessage)){
+                throw new ArgumentNullException("No message string provided");
+            }
             try {
-                string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strMessage);
+                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strMessage);
 
-                var request = new HttpRequestMessage(HttpMethod.Get, url);
-                var result = this._http.SendAsync(request).Result;
+                var request = new HttpRequestMessage (HttpMethod.Get, url);
+                var result = this._http.SendAsync (request).Result;
                 if (result.IsSuccessStatusCode) {
                     return true;
                 } else {
@@ -38,15 +49,21 @@ namespace DotNetStandard_Telegram {
         /// <param name="strMessage">Message content to be sent</param>
         /// <param name="strSource">Source of the message</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool sendMessage(string strMessage, string strSource) {
+        public bool sendMessage (string strMessage, string strSource) {
+            if(String.IsNullOrEmpty(strMessage)){
+                throw new ArgumentNullException("No message string provided");
+            }
+            if(String.IsNullOrEmpty(strSource)){
+                throw new ArgumentNullException("No source provided");
+            }
             try {
                 string strFinalMessage = "Message from: " + strSource;
                 strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
 
-                string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
+                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
 
-                var request = new HttpRequestMessage(HttpMethod.Get, url);
-                var result = this._http.SendAsync(request).Result;
+                var request = new HttpRequestMessage (HttpMethod.Get, url);
+                var result = this._http.SendAsync (request).Result;
                 if (result.IsSuccessStatusCode) {
                     return true;
                 } else {
@@ -64,16 +81,22 @@ namespace DotNetStandard_Telegram {
         /// <param name="strSource">Source of the message</param>
         /// <param name="severity">Severity of message</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool sendMessage(string strMessage, string strSource, Severity severity) {
+        public bool sendMessage (string strMessage, string strSource, Severity severity) {
+            if(String.IsNullOrEmpty(strMessage)){
+                throw new ArgumentNullException("No message string provided");
+            }
+            if(String.IsNullOrEmpty(strSource)){
+                throw new ArgumentNullException("No source provided");
+            }
             try {
                 string strFinalMessage = "Message from: " + strSource;
                 strFinalMessage += Environment.NewLine + "Severity: " + severity;
                 strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
 
-                string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
+                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
 
-                var request = new HttpRequestMessage(HttpMethod.Get, url);
-                var result = this._http.SendAsync(request).Result;
+                var request = new HttpRequestMessage (HttpMethod.Get, url);
+                var result = this._http.SendAsync (request).Result;
                 if (result.IsSuccessStatusCode) {
                     return true;
                 } else {
@@ -83,8 +106,7 @@ namespace DotNetStandard_Telegram {
                 throw ex;
             }
         }
-        public enum Severity
-        {
+        public enum Severity {
             Information = 0,
             Trace = 1,
             Error = 2
