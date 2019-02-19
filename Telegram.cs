b1538870c6915b9a0022408f96092103e17aa223
MThomas564 <38511675+MThomas564@users.noreply.github.com>
2019-02-19 08:19:37 +0000

    updates from feedback

--- Telegram.cs
+++ Telegram.cs
@@ -5,17 +5,25 @@ namespace DotNetStandard_Telegram {
     public class Telegram {
         private string _bot;
         private string _group;
-        private HttpClient _http = new HttpClient ();
-        public Telegram (string strBot, string strGroup) {
-            if (!String.IsNullOrEmpty (strBot)) {
+        private HttpClient _http;
+        public Telegram(HttpClient httpClient, string strBot, string strGroup) {
+            if (httpClient != null) {
+                this._http = httpClient;
+            } else {
+                throw new ArgumentNullException("No HttpClient provided");
+            }
+            if (!String.IsNullOrEmpty(strBot)) {
+                if (!strBot.Contains("bot")) {
+                    throw new ArgumentNullException("bot api key is not correct.");
+                }
                 this._bot = strBot;
             } else {
-                throw new ArgumentNullException ("No bot api key provided");
+                throw new ArgumentNullException("No bot api key provided");
             }
-            if (!String.IsNullOrEmpty (strGroup)) {
+            if (!String.IsNullOrEmpty(strGroup)) {
                 this._group = strGroup;
             } else {
-                throw new ArgumentNullException ("No group ID provided");
+                throw new ArgumentNullException("No group ID provided");
             }
         }
 
@@ -24,22 +32,19 @@ namespace DotNetStandard_Telegram {
         /// </summary>
         /// <param name="strMessage">Message content to be sent</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool sendMessage (string strMessage) {
-            if(string.IsNullOrEmpty(strMessage)){
+        public async System.Threading.Tasks.Task<string> sendMessageAsync(string strMessage) {
+            if (string.IsNullOrEmpty(strMessage)) {
                 throw new ArgumentNullException("No message string provided");
             }
-            try {
-                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strMessage);
+            string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strMessage);
 
-                var request = new HttpRequestMessage (HttpMethod.Get, url);
-                var result = this._http.SendAsync (request).Result;
-                if (result.IsSuccessStatusCode) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (Exception ex) {
-                throw ex;
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await this._http.SendAsync(request);
+            result.EnsureSuccessStatusCode();
+            if (result.IsSuccessStatusCode) {
+                return "success";
+            } else {
+                return "an error has occured. Http status code: " + result.StatusCode;
             }
         }
 
@@ -49,28 +54,25 @@ namespace DotNetStandard_Telegram {
         /// <param name="strMessage">Message content to be sent</param>
         /// <param name="strSource">Source of the message</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool sendMessage (string strMessage, string strSource) {
-            if(String.IsNullOrEmpty(strMessage)){
+        public async System.Threading.Tasks.Task<string> sendMessageAsync(string strMessage, string strSource) {
+            if (String.IsNullOrEmpty(strMessage)) {
                 throw new ArgumentNullException("No message string provided");
             }
-            if(String.IsNullOrEmpty(strSource)){
+            if (String.IsNullOrEmpty(strSource)) {
                 throw new ArgumentNullException("No source provided");
             }
-            try {
-                string strFinalMessage = "Message from: " + strSource;
-                strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
+            string strFinalMessage = "Message from: " + strSource;
+            strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
 
-                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
+            string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
 
-                var request = new HttpRequestMessage (HttpMethod.Get, url);
-                var result = this._http.SendAsync (request).Result;
-                if (result.IsSuccessStatusCode) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (Exception ex) {
-                throw ex;
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await this._http.SendAsync(request);
+            result.EnsureSuccessStatusCode();
+            if (result.IsSuccessStatusCode) {
+                return "success";
+            } else {
+                return "an error has occured. Http status code: " + result.StatusCode;
             }
         }
 
@@ -81,29 +83,26 @@ namespace DotNetStandard_Telegram {
         /// <param name="strSource">Source of the message</param>
         /// <param name="severity">Severity of message</param>
         /// <returns>bool. True if successful, false if not</returns>
-        public bool sendMessage (string strMessage, string strSource, Severity severity) {
-            if(String.IsNullOrEmpty(strMessage)){
+        public async System.Threading.Tasks.Task<string> sendMessageAsync(string strMessage, string strSource, Severity severity) {
+            if (String.IsNullOrEmpty(strMessage)) {
                 throw new ArgumentNullException("No message string provided");
             }
-            if(String.IsNullOrEmpty(strSource)){
+            if (String.IsNullOrEmpty(strSource)) {
                 throw new ArgumentNullException("No source provided");
             }
-            try {
-                string strFinalMessage = "Message from: " + strSource;
-                strFinalMessage += Environment.NewLine + "Severity: " + severity;
-                strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
+            string strFinalMessage = "Message from: " + strSource;
+            strFinalMessage += Environment.NewLine + "Severity: " + severity;
+            strFinalMessage += Environment.NewLine + "Message as follows: " + strMessage;
 
-                string url = String.Format ("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
+            string url = String.Format("https://api.telegram.org/{0}/sendMessage?chat_id={1}&text={2}", this._bot, this._group, strFinalMessage);
 
-                var request = new HttpRequestMessage (HttpMethod.Get, url);
-                var result = this._http.SendAsync (request).Result;
-                if (result.IsSuccessStatusCode) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (Exception ex) {
-                throw ex;
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var result = await this._http.SendAsync(request);
+            result.EnsureSuccessStatusCode();
+            if (result.IsSuccessStatusCode) {
+                return "success";
+            } else {
+                return "an error has occured. Http status code: " + result.StatusCode;
             }
         }
         public enum Severity {
